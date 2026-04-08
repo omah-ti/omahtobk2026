d6d7da99ea8a57bc6103f3b1a26c179cf9ac155f
Yes-07 <nichola.rucita12@gmail.com>
2026-04-08 19:45:13 +0700

    feat(forget password): revamped the forget password page

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ const AuthLayout = async ({ children }: { children?: React.ReactNode }) => {
   const user = await fetchUser()
   if (user) redirect('/')
 
-  return <main className='bg-primary-100 md:h-screen'>{children}</main>
+  return <main className='bg-primary-200 h-screen min-h-fit'>{children}</main>
 }
 
 export default AuthLayout
